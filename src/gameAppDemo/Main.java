233dce316a7aaf1c47b5bd42e107c85ac4f9b982
duygubalci <duygu.balci19@gmail.com>
2021-10-30 12:28:04 +0300

    second commit

--- src/gameAppDemo/Main.java
+++ src/gameAppDemo/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		GameManager gameManager= new GameManager();
 		SaleManager saleManager = new SaleManager();
 		
-		Player player = new Player("24709574334","Duygu", "Balcı", 1990);
+		Player player = new Player("24711111111","Duygu", "Balcı", 1990);
 		playerManager.save(player);
 	
 		Player player2 = new Player("28861499108","Engin", "Demiroğ", 1985);
@@ -24,7 +24,6 @@ public class Main {
 		
 		Discount discount = new Discount("İndirim", 50.2);		
 		
-		gameManager.buyGame(player, game1);
 		gameManager.addBasket(player2, game2);
 		gameManager.addFavourite(player, game2);
 		saleManager.sale(player2, game2, discount);
